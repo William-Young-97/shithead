06ff0483f7cfca023ae7f16dae64add95df5c51a
William Young <wadyoung@outlook.com>
2025-02-01 19:56:30 +0000

    player attr tests fail

--- tests/player_test.py
+++ tests/player_test.py
@@ -2,4 +2,9 @@ from game.player import Player
 
 def test_init():
     player = Player()
+    
     assert isinstance(player, Player)
+    assert player.face_down_cards == []
+    assert player.face_up_cards == []
+    assert player.hand == []
+
