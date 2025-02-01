270d23075379e95c5e482efd5a6bb4afea70d0e8
William Young <wadyoung@outlook.com>
2025-02-01 20:07:01 +0000

    encapsulate player attributes

--- game/player.py
+++ game/player.py
@@ -3,3 +3,13 @@ class Player:
         self.face_down_cards = []
         self.face_up_cards = []
         self.hand = []
+
+    # Helper methods
+    def get_face_down_cards(self):
+        return self.face_down_cards
+    
+    def get_face_up_cards(self):
+        return self.face_up_cards
+    
+    def get_hand(self):
+        return self.hand
--- tests/player_test.py
+++ tests/player_test.py
@@ -2,9 +2,8 @@ from game.player import Player
 
 def test_init():
     player = Player()
-    
-    assert isinstance(player, Player)
-    assert player.face_down_cards == []
-    assert player.face_up_cards == []
-    assert player.hand == []
 
+    assert isinstance(player, Player)
+    assert player.get_face_down_cards() == []
+    assert player.get_face_up_cards() == []
+    assert player.get_hand() == []
