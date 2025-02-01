1189767c7f3232421135ac94c7e513a19d437892
William Young <wadyoung@outlook.com>
2025-02-01 22:52:41 +0000

    test game init 2 players default

--- tests/unit/test_game.py
+++ tests/unit/test_game.py
@@ -1,10 +1,19 @@
 from game.game import Game
 from game.deck import Deck
+from game.player import Player
+
 # Here is a good spot for an integration test to make sure that the deck is shuffled
-# after being init
+
+
 def test_init():
     deck = Deck()
     game = Game(deck)
+    p1 = Player()
+    p2 = Player()
     assert isinstance(game, Game)
     assert isinstance(game.deck, Deck)
-    assert game.get_players() == []
+    assert game.get_players() == [p1, p2]
+
+def test_set_players():
+    deck = Deck()
+    game = Game(deck)
