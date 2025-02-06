bab088c49c512fe5da86e97bb519a391f3bf47e4
William Young <wadyoung@outlook.com>
2025-02-06 18:23:28 +0000

    revert special

--- game/card.py
+++ game/card.py
@@ -1,10 +1,9 @@
 class Card:
-    def __init__(self, rank, suit, value, special=False):
+    def __init__(self, rank, suit, value):
         self.rank = rank
         self.suit = suit
         self.value = value
-        self.special = special
-    
+       
     def __str__(self):
         suit_symbols = {"Spades": "♠", "Hearts": "♥", "Diamonds": "♦", "Clubs": "♣"}
         return f"{self.rank}{suit_symbols[self.suit]}" 
--- tests/unit/test_card.py
+++ tests/unit/test_card.py
@@ -9,5 +9,4 @@ def test_init_normal_card():
 def test_init_special_card():
     card = Card("Ten", "Spades", 10, True)
     assert card.special == True
-    assert card.effect != None
 
