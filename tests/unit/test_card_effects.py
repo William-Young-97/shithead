914ef9424aa1e3b4c707f3624ec2d578f849b82d
William Young <wadyoung@outlook.com>
2025-02-08 20:24:26 +0000

    break 10 test for rank

--- tests/unit/test_card_effects.py
+++ tests/unit/test_card_effects.py
@@ -5,13 +5,11 @@ from tests.helpers import fake_input_sequence
 
 def test_ten_clears_discard_pile():
     game = Game(input_fn=fake_input_sequence(["Bob", "0"]))
-    game.discard_pile = [Card("5", "Clubs", 5)]
+    game.discard_pile = [Card("Jack", "Clubs", 11)]
     player = game.players[0]
     player.hand = [Card("10", "Clubs", 10)]
     player.select_action(game)
     assert len(game.discard_pile) == 0
-
-
 
 def test_seven_reverses_order_of_play():
     game = Game(input_fn=fake_input_sequence(["Bob", "0"]))
